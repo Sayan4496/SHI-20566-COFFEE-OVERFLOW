29b864fe1a3bee82e13dd0a746704cfd97f630e4
Sayan Deyashi <deyashisayan2@gmail.com>
2025-09-22 21:03:38 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,14 @@
+# creating directroies
+mkdir -p src
+mkdir -p research
+# creating files
+touch src/__init__.py
+touch src/helpers.py
+touch src/prompt.py
+touch .env
+touch app.py
+touch research/trial.ipynb
+touch requirements.txt
+touch README.md
+
+echo "Project structure created successfully."
